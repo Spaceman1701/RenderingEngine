c854292c6f6efc5fcffab971edf1853365004238
Spaceman1701 <fc.spaceman@gmail.com>
2016-10-11 03:16:12 -0600

    remove old files

--- RenderingEngine/math_test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-namespace test {
-	namespace math {
-		bool runTests(int* numFailed);
-		bool vector3Test();
-		bool matrix3Test();
-		bool matrix4Test();
-	}
-}
--- RenderingEngine/test.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-namespace test {
-	void runAllTests();
-}
